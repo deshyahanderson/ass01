a271673c5845d7bcb7599d6d436cce6bf839ac35
deshyahanderson <ander3dy@mail.uc.edu>
2025-02-26 15:16:30 -0500

    done

---
+++ src/TicTacToeRunner.java
@@ -0,0 +1,10 @@
+package src;
+
+import javax.swing.SwingUtilities;
+
+public class TicTacToeRunner {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new TicTacToeFrame());
+    }
+}
